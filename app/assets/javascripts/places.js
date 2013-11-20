4fff9e44b9bc07e75748b74502dfd4d66c475388
kathrynsiegel <ksiegel@mit.edu>
2013-11-20 12:50:09 -0500

    fixed js

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -85,94 +85,105 @@ $(document).ready(function() {
 		
 	}
 
-	var ctpop = document.getElementById("population-chart").getContext("2d");
-	var data_population = {
-		labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [3857700, 3880500, 3948500, 4027200, 4087500, 4133900, 4184600, 4228300, 4268900, 4315800, 4367800, 4405200, 4433100]
-			}
-		]
+	var ctpop = document.getElementById("population-chart");
+	if (ctpop) {
+		var data_population = {
+			labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [3857700, 3880500, 3948500, 4027200, 4087500, 4133900, 4184600, 4228300, 4268900, 4315800, 4367800, 4405200, 4433100]
+				}
+			]
+		}
+		var popChart = new Chart(ctpop.getContext("2d")).Line(data_population,defaults);
 	}
-	var popChart = new Chart(ctpop).Line(data_population,defaults);
-
-	var cticap = document.getElementById("icap-chart").getContext("2d");
-	var data_icap = {
-		labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [307.943, 306.277, 312.223, 362.335, 385.948, 369.095, 399.098, 388.673, 333.002, 327.369, 300.289]
-			}
-		]
+	
+	var cticap = document.getElementById("icap-chart");
+	if (cticap) {
+		var data_icap = {
+			labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [307.943, 306.277, 312.223, 362.335, 385.948, 369.095, 399.098, 388.673, 333.002, 327.369, 300.289]
+				}
+			]
+		}
+		var capChart = new Chart(cticap.getContext("2d")).Line(data_icap,defaults);
 	}
-	var capChart = new Chart(cticap).Line(data_icap,defaults);
 
-	var cticap2 = document.getElementById("icap-chart2").getContext("2d");
-	var data_icap2 = {
-		labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [2850.8, 2850.9, 2739.8, 2799.8, 2825.1, 2987.2, 3081.8, 3175.8, 3323.4, 3187.8]
-			}
-		]
+	var cticap2 = document.getElementById("icap-chart2");
+	if (cticap2) {
+		var data_icap2 = {
+			labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [2850.8, 2850.9, 2739.8, 2799.8, 2825.1, 2987.2, 3081.8, 3175.8, 3323.4, 3187.8]
+				}
+			]
+		}
+		var capChart2 = new Chart(cticap2.getContext("2d")).Line(data_icap2,defaults);
 	}
-	var capChart2 = new Chart(cticap2).Line(data_icap2,defaults);
 
-	var ctstem = document.getElementById("stem-chart").getContext("2d");
-	var data_stem = {
-		labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [298, 299, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300]
-			}
-		]
+	var ctstem = document.getElementById("stem-chart");
+	if (ctstem) {
+		var data_stem = {
+			labels : ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [298, 299, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300]
+				}
+			]
+		}
+		var stemChart = new Chart(ctstem.getContext("2d")).Line(data_stem,defaults);
 	}
-	var stemChart = new Chart(ctstem).Line(data_stem,defaults);
 
-	var ctrd = document.getElementById("rd-chart").getContext("2d");
-	var data_rd = {
-		labels : ["2001","2003","2005","2007","2009"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [1.12108, 1.16896, 1.13693, 1.18567, 1.30137]
-			}
-		]
+	var ctrd = document.getElementById("rd-chart");
+	if (ctrd) {
+		var data_rd = {
+			labels : ["2001","2003","2005","2007","2009"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [1.12108, 1.16896, 1.13693, 1.18567, 1.30137]
+				}
+			]
+		}
+		var rdChart = new Chart(ctrd.getContext("2d")).Line(data_rd,defaults);
 	}
-	var rdChart = new Chart(ctrd).Line(data_rd,defaults);
-
-	var ctiprop = document.getElementById("iprop-chart").getContext("2d");
-	var data_iprop = {
-		labels : ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [5.29444, 5.60356, 5.62319, 6.01035, 5.87085, 6.19631, 5.7669, 5.78175, 6.06878, 5.61601, 5.96089]
-			}
-		]
+		
+	var ctiprop = document.getElementById("iprop-chart");
+	if (ctiprop) {
+		var data_iprop = {
+			labels : ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [5.29444, 5.60356, 5.62319, 6.01035, 5.87085, 6.19631, 5.7669, 5.78175, 6.06878, 5.61601, 5.96089]
+				}
+			]
+		}
+		var ipropChart = new Chart(ctiprop.getContext("2d")).Line(data_iprop,defaults);
 	}
-	var ipropChart = new Chart(ctiprop).Line(data_iprop,defaults);
-
 })
