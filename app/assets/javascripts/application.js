d8741c466124076e8ec4311167184f4eb4bdbe6e
kathrynsiegel <ksiegel@mit.edu>
2013-12-03 19:07:18 -0500

    fixed main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,58 +21,60 @@ var hasClass = function(element, cls) {
 }
 
 $(document).ready(function() {
-	$('.partner-country-index.light-blue-reap').mouseover(function() {
-		$(this).removeClass('light-blue-reap');
-		$(this).addClass('pink-reap');
+	$('.partner-country-index').mouseover(function() {
+		jQuery($(this).children()[0]).removeClass('light-blue-reap');
+		jQuery($(this).children()[0]).addClass('pink-reap');
 	})
 
-	$('.partner-country-index.light-blue-reap').mouseout(function() {
-		$(this).removeClass('pink-reap');
-		$(this).addClass('light-blue-reap');
+	$('.partner-country-index').mouseout(function() {
+		jQuery($(this).children()[0]).removeClass('pink-reap');
+		jQuery($(this).children()[0]).addClass('light-blue-reap');
 	})
 
-	$('.partner-country-index.dark-blue-reap').mouseover(function() {
-		$(this).removeClass('dark-blue-reap');
-		$(this).addClass('red-reap');
+	$('.partner-country-index').mouseover(function() {
+		jQuery($(this).children()[0]).removeClass('dark-blue-reap');
+		jQuery($(this).children()[0]).addClass('red-reap');
 	})
 
-	$('.partner-country-index.dark-blue-reap').mouseout(function() {
-		$(this).removeClass('red-reap');
-		$(this).addClass('dark-blue-reap');
+	$('.partner-country-index').mouseout(function() {
+		jQuery($(this).children()[0]).removeClass('red-reap');
+		jQuery($(this).children()[0]).addClass('dark-blue-reap');
 	})
 
-	$('.partner-country-index.pink-reap').mouseover(function() {
-		$(this).removeClass('pink-reap');
-		$(this).addClass('light-blue-reap');
+	$('.person-holder.pink-reap').mouseover(function() {
+		jQuery($(this).children()[0]).removeClass('pink-reap');
+		jQuery($(this).children()[0]).addClass('light-blue-reap');
 	})
 
-	$('.partner-country-index.pink-reap').mouseout(function() {
-		$(this).removeClass('light-blue-reap');
-		$(this).addClass('pink-reap');
+	$('.person-holder.pink-reap').mouseout(function() {
+		jQuery($(this).children()[0]).removeClass('light-blue-reap');
+		jQuery($(this).children()[0]).addClass('pink-reap');
 	})
 
-	$('.partner-country-index.red-reap').mouseover(function() {
-		$(this).removeClass('red-reap');
-		$(this).addClass('dark-blue-reap');
+	$('.person-holder.red-reap').mouseover(function() {
+		jQuery($(this).children()[0]).removeClass('red-reap');
+		jQuery($(this).children()[0]).addClass('dark-blue-reap');
 	})
 
-	$('.partner-country-index.red-reap').mouseout(function() {
-		$(this).removeClass('dark-blue-reap');
-		$(this).addClass('red-reap');
+	$('.person-holder.red-reap').mouseout(function() {
+		jQuery($(this).children()[0]).removeClass('dark-blue-reap');
+		jQuery($(this).children()[0]).addClass('red-reap');
 	})
 
-	$('.person-circle').mouseover(function() {
-		jQuery($(this).children()[1]).css('visibility','visible');	
-		jQuery($(this).children()[2]).css('visibility','visible');		
+	$('.person-holder').mouseover(function() {
+		jQuery($(this).children()[2]).css('visibility','visible');	
+		jQuery($(this).children()[3]).css('visibility','visible');		
 	})
 
-	$('.person-circle').mouseout(function() {
-		jQuery($(this).children()[1]).css('visibility','hidden');
+	$('.person-holder').mouseout(function() {
 		jQuery($(this).children()[2]).css('visibility','hidden');
+		jQuery($(this).children()[3]).css('visibility','hidden');
 	})
 
 	$('#partner-regions-link').bind("ajax:after", function(evt, xhr, settings){
     	window.scroll(0, 1000);
     })
+
+    $('#cohort2show').popover('hide')
 })
 
